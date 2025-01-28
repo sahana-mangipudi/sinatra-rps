8a840389b1cfa7fb5b79a772a3151c69a47f9671
sahana-mangipudi <smangipudi@uchicago.edu>
2025-01-28 16:43:25 +0000

    forked from repository, created instances and erb file

--- app.rb
+++ app.rb
@@ -3,7 +3,23 @@ require "sinatra/reloader"
 
 get("/") do
   "
-  <h1>Welcome to your Sinatra App!</h1>
+  <h1>Howdy!</h1>
   <p>Define some routes in app.rb</p>
   "
+end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+  comp_move = moves.sample
+  if comp_move == "rock"
+    @outcome = "tied"
+  elseif comp_move == "paper"
+    @outcome = "lost"
+  else 
+    @outcome = "scissors"
+  end 
+ 
+end
+
+  erb(:zebra)
 end
